11153b4e5873a1c44d6905e7473cdb38802995d6
Sigitora <is50_r.s.matiev@mpt.ru>
2023-11-09 18:30:57 +0300

    добавление файлов проекта

---
+++ js/script.js
@@ -0,0 +1,5 @@
+function toggleMenu() {
+    let menuList = document.getElementById("list");
+    menuList.classList.toggle("active");
+    document.getElementById("menu-bar").classList.toggle("change");
+}
